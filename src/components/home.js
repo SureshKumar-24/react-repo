6487f4e64535fc994e6830d98357e2d0549fc7d7
mohd usman <usmanenact@gmail.com>
2024-02-26 18:26:40 +0530

    redux

---
+++ src/components/home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <div className="add-to-cart">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+            </div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://cdn.dxomark.com/wp-content/uploads/medias/post-125834/Apple-iPhone-14_FINAL_featured-image-packshot-review.jpg" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
